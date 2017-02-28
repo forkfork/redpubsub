162668c47369e64f964e00c1d72c4454cc33376a
Timothy D <timothydowns@gmail.com>
2017-02-28 17:02:10 +1100

    add scores implementation

---
+++ livepub.lua
@@ -0,0 +1,38 @@
+local _M = {}
+
+_M.subscribe = function(redis)
+  local res, err = redis:get("snapshot:scores")
+  ngx.say(res)
+  ngx.flush()
+  redis:subscribe("delta:scores")
+  while not err do
+    res, err = redis:read_reply()
+    if res then
+      ngx.log(ngx.ERR, "subject is " .. res[2])
+      ngx.log(ngx.ERR, "details is " .. res[3])
+      ngx.say(res[3])
+      ngx.flush()
+    else
+      if err == "timeout" then
+        ngx.log(ngx.ERR, "TIMEOUT")
+        break
+      else
+        ngx.log(ngx.ERR, "OTHER ERROR")
+        ngx.log(ngx.ERR, err)
+        break
+      end
+    end
+  end
+end
+
+_M.delta = function(redis)
+  ngx.req.read_body()
+  local body_data = ngx.req.get_body_data()
+  redis:publish("delta:scores", body_data)
+end
+
+_M.snapshot = function(redis)
+  ngx.req.read_body()
+  local body_data = ngx.req.get_body_data()
+  redis:publish("delta:snapshot", body_data)
+end
